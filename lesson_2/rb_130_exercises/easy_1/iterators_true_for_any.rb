673e2def24d41aa384619883ab6e5283a83dc4ca
asherorr <103453685+asherorr@users.noreply.github.com>
2025-03-07 17:40:33 +0000

    This commit will add exercises 5-10 in the RB130 'Easy 1' problem set.

---
+++ lesson_2/rb_130_exercises/easy_1/iterators_true_for_any.rb
@@ -0,0 +1,18 @@
+#Write a method that takes an array as an argument performs the same function
+#as Enumerable#any?
+
+def any?(array)
+  return false if array.empty?
+
+  array.each { |item| return true if yield(item) }
+
+  false
+end
+
+p any?([1, 3, 5, 6]) { |value| value.even? } == true
+p any?([1, 3, 5, 7]) { |value| value.even? } == false
+p any?([2, 4, 6, 8]) { |value| value.odd? } == false
+p any?([1, 3, 5, 7]) { |value| value % 5 == 0 } == true
+p any?([1, 3, 5, 7]) { |value| true } == true
+p any?([1, 3, 5, 7]) { |value| false } == false
+p any?([]) { |value| true } == false
---
+++ lesson_2/rb_130_exercises/easy_1/iterators_true_for_none.rb
@@ -0,0 +1,17 @@
+#Write a method that takes an array as an argument performs the same function
+#as Enumerable#none?
+
+def none?(array)
+  return true if array.empty?
+
+  array.each { |item| return false if yield(item) }
+  true
+end
+
+p none?([1, 3, 5, 6]) { |value| value.even? } == false
+p none?([1, 3, 5, 7]) { |value| value.even? } == true
+p none?([2, 4, 6, 8]) { |value| value.odd? } == true
+p none?([1, 3, 5, 7]) { |value| value % 5 == 0 } == false
+p none?([1, 3, 5, 7]) { |value| true } == false
+p none?([1, 3, 5, 7]) { |value| false } == true
+p none?([]) { |value| true } == true
