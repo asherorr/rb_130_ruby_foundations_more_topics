492859c43cbb3a71d41d76f8148fe51a8e510d4d
asherorr <103453685+asherorr@users.noreply.github.com>
2025-03-30 17:18:37 +0000

    This commit will add challenge 2.

---
+++ challenges/easy_challenges/challenge_2/point_mutations.rb
@@ -0,0 +1,18 @@
+class DNA
+  attr_reader :original_strand
+
+  def initialize(original_strand)
+    @original_strand = original_strand
+  end
+
+  def hamming_distance(test_strand)
+    distance = 0
+
+    minimum_size = [original_strand.length, test_strand.length].min
+    (0...minimum_size).each do |idx|
+      distance += 1 if original_strand.chars[idx] != test_strand.chars[idx]
+    end
+
+    distance
+  end
+end
