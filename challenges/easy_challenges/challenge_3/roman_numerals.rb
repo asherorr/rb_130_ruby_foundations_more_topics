982c4ead6090863f622b1cccf737a7d93c730e2b
asherorr <103453685+asherorr@users.noreply.github.com>
2025-04-02 00:15:29 +0000

    This commit will add challenge 3.

---
+++ challenges/easy_challenges/challenge_3/roman_numerals.rb
@@ -0,0 +1,29 @@
+
+class RomanNumeral
+  CONVERSION = {
+    1 => ["I", "X", "C", "M"],
+    2 => ["II", "XX", "CC", "MM"],
+    3 => ["III", "XXX", "CCC", "MMM"],
+    4 => ["IV", "XL", "CD"],
+    5 => ["V", "L", "D"],
+    6 => ["VI", "LX", "DC"],
+    7 => ["VII", "LXX", "DCC"],
+    8 => ["VIII", "LXXX", "DCCC"],
+    9 => ["IX", "XC", "CM"],
+    0 => ""
+  }
+
+  def initialize(number)
+    @number = number
+  end
+
+  def to_roman
+    number_reversed = @number.digits
+
+    roman_numerals_array = number_reversed.map.with_index do |digit, idx|
+      CONVERSION[digit][idx]
+    end
+
+    roman_numerals_array.reverse.join
+  end
+end
